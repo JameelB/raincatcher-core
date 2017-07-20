a372b0b734a9135ca1e12e50b9465f43c9e5e2f9
Jameel Briones <jbriones@redhat.com>
2017-07-20 16:21:47 +0100

    Added custom access denied handler for keycloak

--- demo/server/src/modules/keycloak/index.ts
+++ demo/server/src/modules/keycloak/index.ts
@@ -15,8 +15,13 @@ export function init(app: express.Express) {
   const memoryStore = new session.MemoryStore();
   const keycloak = new Keycloak({ store: memoryStore });
 
+  // Set custom access denied handler
+  keycloak.accessDenied = function(req: express.Request, res: express.Response) {
+    res.redirect('/access-denied');
+  };
+
   // Use keycloak middleware & define applications logout route
-  app.use(keycloak.middleware( { logout: '/logout'} ));
+  app.use(keycloak.middleware({logout: '/logout'}));
 
   // return the keycloak middleware
   return keycloak;
